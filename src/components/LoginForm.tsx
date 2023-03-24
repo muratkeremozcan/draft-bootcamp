ec824c2ff014a1d4107bf5a9c8e6fca56f17d189
Murat Ozcan <muratkerem@gmail.com>
2023-03-24 14:22:36 -0500

    fixed data-cy

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -5,7 +5,7 @@ import Button from './Button'
 
 export default function LoginForm() {
   return (
-    <form className="login-form">
+    <form data-cy="LoginForm" className="login-form">
       <Logo />
       <Input id="email" label="Email Address" name="email" />
       <PasswordInput id="passwordToggle" label="Password" />
