1e657fb9cb671c4dc4d98787ab138c9a971716d7
Murat Ozcan <muratkerem@gmail.com>
2023-03-27 08:24:46 -0500

    lesson 3

--- src/components/LoginForm.cy.tsx
+++ src/components/LoginForm.cy.tsx
@@ -5,11 +5,31 @@ describe('LoginForm', () => {
     cy.mount(<LoginForm />)
 
     cy.get('img').should('be.visible')
-
-    cy.get('#email').type('test@example.com')
-    cy.getByCy('PasswordInput').type('123456')
-
     cy.contains('#passwordToggle', 'Show').should('be.visible')
-    cy.contains('button', 'Log in').should('be.visible')
+
+    cy.on('window:alert', cy.spy().as('alert'))
+
+    cy.log('**email empty**')
+    cy.contains('button', 'Log in').click()
+    cy.get('@alert').should(
+      'have.been.calledWith',
+      'Please enter a non-empty email',
+    )
+
+    cy.log('**password empty**')
+    cy.get('#email').type('test@example.com')
+    cy.contains('button', 'Log in').click()
+    cy.get('@alert').should(
+      'have.been.calledWith',
+      'Please enter a non-empty password',
+    )
+
+    cy.log('**both filled**')
+    cy.getByCy('PasswordInput').type('123456')
+    cy.contains('button', 'Log in').click()
+    cy.get('@alert').should(
+      'have.been.calledWith',
+      'Login submitted successfully!',
+    )
   })
 })
